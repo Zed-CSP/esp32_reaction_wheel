661281a8fba32f17de7bb1539331d6b76e932d81
Christopher Peret <cperet89@gmail.com>
2023-07-02 12:31:18 -0700

    define encoders

--- develop/main.h
+++ develop/main.h
@@ -1,3 +1,5 @@
+
+// axis x, y, z
 #define DIR_x        4
 #define PWM_x       32
 #define PWM_x_CH     1
@@ -9,3 +11,15 @@
 #define DIR_z        5
 #define PWM_z        18
 #define PWM_z_CH     2
+
+// encoder
+
+#define ENC_x_A      34
+#define ENC_x_B      35
+
+#define ENC_y_A      26
+#define ENC_y_B      27
+
+#define ENC_z_A      14
+#define ENC_z_B      12
+
