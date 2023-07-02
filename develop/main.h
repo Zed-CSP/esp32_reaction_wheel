577ef8fd6b1ca3e4901ac0c6c9cde5308cd1b565
Christopher Peret <cperet89@gmail.com>
2023-07-02 12:29:01 -0700

    define x, y, z, axis

---
+++ develop/main.h
@@ -0,0 +1,11 @@
+#define DIR_x        4
+#define PWM_x       32
+#define PWM_x_CH     1
+
+#define DIR_y        15
+#define PWM_y        25
+#define PWM_y_CH     0
+
+#define DIR_z        5
+#define PWM_z        18
+#define PWM_z_CH     2
