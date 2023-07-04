9c6635ce5d1d9eca61e5596aafb9b8dc99f16758
Christopher Peret <cperet89@gmail.com>
2023-07-03 23:24:52 -0700

    init esp32.ino

---
+++ develop/esp32.ino
@@ -0,0 +1,34 @@
+#include "main.h"
+#include EEPROM.h
+#include " BluetoothSerial.h"
+
+// Bluetooth
+BluetoothSerial SerialBT; // Bluetooth Serial object
+
+// EEPROM
+
+// Motor
+
+// IMU
+
+// PID
+
+// Timer
+
+// Encoder
+
+// Accessories
+
+// Serial
+
+// Global variables
+
+// Functions
+
+// Setup
+
+// Loop
+
+// Interrupts
+
+// Bluetooth
